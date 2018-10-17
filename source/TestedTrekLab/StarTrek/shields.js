2f8d34812351a339801fc05dcb16eda2c227135d
Doug Falk <Falk.Doug@mayo.edu>
2018-10-17 16:15:25 -0500

    User Story 1

--- source/TestedTrekLab/StarTrek/shields.js
+++ source/TestedTrekLab/StarTrek/shields.js
@@ -1,7 +1,22 @@
-Shield = function() {};
+Shield = function() {
+  this.up = false;
+  this.energyRemaining = 9001;
+  this.maxShieldEnergy = 10000;
+  this.minShieldEnergy = 0;
+};
 
 Shield.prototype = {
-  isUp: () => {
-    return false;
+  get isUp() {
+    return this.up;
+  },
+  get remainingEnergy() {
+    return this.energyRemaining;
+  },
+  transferEnergy: function(energyToTransfer) {
+    this.energyRemaining = Math.max(this.minShieldEnergy, Math.min(this.energyRemaining + energyToTransfer, this.maxShieldEnergy))
   }
+};
+
+Shield.prototype.setShield = function (val) {
+    this.up = val;
 };
--- source/TestedTrekLab/Tests/shields.test.js
+++ source/TestedTrekLab/Tests/shields.test.js
@@ -1,6 +1,38 @@
 describe("shield", () => {
-  it("starts out lowered", () => {
-    const shield = new Shield();
-    expect(shield.isUp()).toBeFalsy();
-  });
+    it("starts out lowered", () => {
+        const shield = new Shield();
+        expect(shield.isUp).toBeFalsy();
+    });
+
+    it("can be raised", () => {
+        const shield = new Shield();
+        expect(shield.isUp).toBeFalsy();
+        shield.setShield(true);
+        expect(shield.isUp).toBeTruthy();
+    });
+
+    it('should start with an energy level of of 9001', () => {
+        const shield = new Shield();
+        expect(shield.remainingEnergy).toEqual(9001);
+    });
+
+    it('transfers energy to the shield', () => {
+        const shield = new Shield();
+        expect(shield.remainingEnergy).toEqual(9001);
+        shield.transferEnergy(4);
+        expect(shield.remainingEnergy).toEqual(9005);
+    });
+
+    it('should max out at 10000 energy', () => {
+        const shield = new Shield();
+        shield.transferEnergy(1000);
+        expect(shield.remainingEnergy).toEqual(10000);
+    })
+
+    it('should min out at 0 energy', () => {
+        const shield = new Shield();
+        expect(shield.remainingEnergy).toEqual(9001);
+        shield.transferEnergy(-49872);
+        expect(shield.remainingEnergy).toEqual(0);
+    })
 });
