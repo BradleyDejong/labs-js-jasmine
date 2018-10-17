00cdd39bae0e0c05aa34d244c0da1f599aa3de67
Bradley DeJong <DeJong.Bradley@mayo.edu>
2018-10-17 14:03:19 -0500

    Move weapons into separate classes

--- source/TestedTrekLab/StarTrek/game.js
+++ source/TestedTrekLab/StarTrek/game.js
@@ -1,99 +1,147 @@
-Game = function () {
-    this.energy = 10000;
-    this.torpedoes = 8;
-    this.maxPhaserRange = 4000;
+Weapon = function(randomGenerator) {
+  this.randomWithinLimitOf = n => {
+    return Math.floor(randomGenerator.generator() * n);
+  };
+};
+
+Weapon.prototype = {
+  fire: () => {
+    throw new Error("Implement me (fire)");
+  },
+  applyDamageToTarget: function(enemy, damage) {
+    var messages = [];
+    if (damage < enemy.energy) {
+      enemy.energy = enemy.energy - damage;
+      messages.push("Klingon has " + enemy.energy + " remaining");
+    } else {
+      messages.push("Klingon destroyed!");
+      enemy.destroy();
+    }
+    return messages;
+  }
+};
+
+Phasers = function(randomGenerator) {
+  Weapon.call(this, randomGenerator);
+};
+Phasers.prototype = Object.create(Weapon.prototype);
+Phasers.prototype.maxPhaserRange = 4000;
+Phasers.prototype.fire = function(ship, enemy, ui) {
+  var amount = parseInt(ui.parameter("amount"), 10);
+
+  const canFire = () => ship.energy >= amount;
+
+  if (canFire()) {
+    return this.firePhaserAt(ship, enemy, amount);
+  } else {
+    return ["Insufficient energy to fire phasers!"];
+  }
+};
+
+Phasers.prototype.firePhaserAt = function(ship, enemy, amount) {
+  const distance = enemy.distance;
+  var messages = [];
+  if (distance > this.maxPhaserRange) {
+    messages.push(
+      "Klingon out of range of phasers at " + distance + " sectors..."
+    );
+  } else {
+    var damage = this.calculateDamageByDistance(amount, distance);
+
+    messages.push(
+      "Phasers hit Klingon at " +
+        distance +
+        " sectors with " +
+        damage +
+        " units"
+    );
+    messages = messages.concat(this.applyDamageToTarget(enemy, damage));
+  }
+  ship.energy -= amount;
+  return messages;
+};
+
+Phasers.prototype.calculateDamageByDistance = function(maxDamage, distance) {
+  const damageReductionByDistance = ((maxDamage / 20) * distance) / 200;
+  const qualityOfStrikePenalty = this.randomWithinLimitOf(200);
+  const finalDamage =
+    maxDamage - (damageReductionByDistance + qualityOfStrikePenalty);
+  return finalDamage < 1 ? 1 : finalDamage;
+};
+
+Torpedoes = function(gen) {
+  Weapon.call(this, gen);
+};
+Torpedoes.prototype = Object.create(Weapon.prototype);
+
+Torpedoes.prototype.fire = function(ship, enemy) {
+  const canFire = () => ship.torpedoes > 0;
+  if (canFire()) {
+    return this.fireTorpedoAt(ship, enemy);
+  } else {
+    return ["No more photon torpedoes!"];
+  }
+};
+
+Torpedoes.prototype.fireTorpedoAt = function(ship, enemy) {
+  var messages = [];
+  var distance = enemy.distance;
+  if (this.randomWithinLimitOf(4) + (distance / 500 + 1) > 7) {
+    messages.push("Torpedo missed Klingon at " + distance + " sectors...");
+  } else {
+    var damage = 800 + this.randomWithinLimitOf(50);
+    messages.push(
+      "Photons hit Klingon at " +
+        distance +
+        " sectors with " +
+        damage +
+        " units"
+    );
+    messages = messages.concat(this.applyDamageToTarget(enemy, damage));
+  }
+  ship.torpedoes--;
+  return messages;
+};
+
+Game = function() {
+  this.energy = 10000;
+  this.torpedoes = 8;
 };
 
 Game.prototype = {
-    generator: function () {
-        return Math.random();
-    },
-    randomWithinLimitOf: function (n) {
-        return Math.floor(this.generator() * n);
-    },
-    calculateDamageByDistance: function (maxDamage, distance) {
-        const damageReductionByDistance = ((maxDamage / 20) * distance) / 200;
-        const qualityOfStrikePenalty = this.randomWithinLimitOf(200);
-        const finalDamage = (maxDamage - (damageReductionByDistance + qualityOfStrikePenalty));
-        return finalDamage < 1 ? 1 : finalDamage;
-    },
-    applyDamageToTarget: function (enemy, damage) {
-        var messages = [];
-        if (damage < enemy.energy) {
-            enemy.energy = enemy.energy - damage;
-            messages.push("Klingon has " + enemy.energy + " remaining");
-        } else {
-            messages.push("Klingon destroyed!");
-            enemy.destroy();
-        }
-        return messages;
-    },
-
-    subtractEnergyFromMeAndApplyDamangeToEnemy: function (enemy, amount) {
-        const distance = enemy.distance;
-        var messages = [];
-        if (distance > this.maxPhaserRange) {
-            messages.push(
-                "Klingon out of range of phasers at " + distance + " sectors..."
-            );
-        } else {
-            var damage = this.calculateDamageByDistance(amount, distance);
-
-            messages.push(
-                "Phasers hit Klingon at " +
-                distance +
-                " sectors with " +
-                damage +
-                " units"
-            );
-            messages = messages.concat(this.applyDamageToTarget(enemy, damage));
-        }
-        this.energy -= amount;
-        return messages;
-    },
-    fireTorpedoAt: function (enemy) {
-        var messages = [];
-        var distance = enemy.distance;
-        if (this.randomWithinLimitOf(4) + (distance / 500 + 1) > 7) {
-            messages.push("Torpedo missed Klingon at " + distance + " sectors...");
-        } else {
-            var damage = 800 + this.randomWithinLimitOf(50);
-            messages.push(
-                "Photons hit Klingon at " +
-                distance +
-                " sectors with " +
-                damage +
-                " units"
-            );
-            if (damage < enemy.energy) {
-                enemy.energy = enemy.energy - damage;
-                messages.push("Klingon has " + enemy.energy + " remaining");
-            } else {
-                messages.push("Klingon destroyed!");
-                enemy.destroy();
-            }
-        }
-        this.torpedoes--;
-        return messages;
-    },
-    processCommand: function (ui) {
-        if (ui.parameter("command") === "phaser") {
-            var amount = parseInt(ui.parameter("amount"), 10);
-            if (this.energy >= amount) {
-                this.subtractEnergyFromMeAndApplyDamangeToEnemy(ui.variable("target"), amount).forEach(function (x) {
-                    ui.writeLine(x);
-                })
-            } else {
-                ui.writeLine("Insufficient energy to fire phasers!");
-            }
-        } else if (ui.parameter("command") === "photon") {
-            if (this.torpedoes > 0) {
-                this.fireTorpedoAt(ui.variable("target")).forEach(function (x) {
-                    ui.writeLine(x);
-                })
-            } else {
-                ui.writeLine("No more photon torpedoes!");
-            }
-        }
+  generator: function() {
+    return Math.random();
+  },
+  randomWithinLimitOf: function(n) {
+    return Math.floor(this.generator() * n);
+  },
+  applyDamageToTarget: function(enemy, damage) {
+    var messages = [];
+    if (damage < enemy.energy) {
+      enemy.energy = enemy.energy - damage;
+      messages.push("Klingon has " + enemy.energy + " remaining");
+    } else {
+      messages.push("Klingon destroyed!");
+      enemy.destroy();
     }
+    return messages;
+  },
+  processCommand: function(ui) {
+    function writeAllMessages(msgs) {
+      msgs.forEach(m => ui.writeLine(m));
+    }
+
+    const myPhasers = new Phasers(this);
+    const myTorpedoes = new Torpedoes(this);
+
+    const weapons = {
+      phaser: myPhasers.fire.bind(myPhasers), //this.tryFirePhaser.bind(this),
+      photon: myTorpedoes.fire.bind(myTorpedoes)
+    };
+
+    const command = ui.parameter("command");
+    const enemy = ui.variable("target");
+
+    writeAllMessages(weapons[command](this, enemy, ui));
+  }
 };
--- source/TestedTrekLab/Tests/phaserCharacterization.test.js
+++ source/TestedTrekLab/Tests/phaserCharacterization.test.js
@@ -1,109 +1,116 @@
 describe("phasers", function() {
-    var game;
-    var ui;
-    var energyBefore;
-    
+  var game;
+  var ui;
+  var energyBefore;
+
+  beforeEach(function() {
+    game = new Game();
+    energyBefore = game.energy;
+    ui = new UserInterface("phaser");
+    spyOn(ui, "writeLine");
+  });
+
+  it("should complain with insufficient energy when not available", function() {
+    // given
+    ui.commandParameter = game.energy + 1;
+
+    // when
+    game.processCommand(ui);
+
+    // then
+    expect(ui.writeLine).toHaveBeenCalledWith(
+      "Insufficient energy to fire phasers!"
+    );
+  });
+
+  describe("when Klingon out of range", function() {
+    var outOfRange;
+    var energyToFire = 1000;
     beforeEach(function() {
-        game = new Game();
-        energyBefore = game.energy;
-        ui = new UserInterface("phaser");
-        spyOn(ui, "writeLine");
+      outOfRange = Phasers.prototype.maxPhaserRange + 1;
+      ui.target = new Klingon(outOfRange);
+      ui.commandParameter = energyToFire;
+      game.processCommand(ui);
     });
 
-    it("should complain with insufficient energy when not available", function() {
-        // given
-        ui.commandParameter = game.energy + 1;
-
-        // when
-        game.processCommand(ui);
-
-        // then
-        expect(ui.writeLine).toHaveBeenCalledWith("Insufficient energy to fire phasers!");
+    it("reports out-of-range", function() {
+      expect(ui.writeLine).toHaveBeenCalledWith(
+        "Klingon out of range of phasers at " + outOfRange + " sectors..."
+      );
     });
 
-    describe("when Klingon out of range", function() {
-        var outOfRange;
-        var energyToFire = 1000;
-        beforeEach(function() {
-            outOfRange = game.maxPhaserRange + 1;
-            ui.target = new Klingon(outOfRange);
-            ui.commandParameter = energyToFire;
-            game.processCommand(ui);
-        });
+    it("still subtracts the energy", function() {
+      expect(game.energy).toBe(energyBefore - energyToFire);
+    });
+  });
 
-        it("reports out-of-range", function() {
-            expect(ui.writeLine).toHaveBeenCalledWith("Klingon out of range of phasers at " + outOfRange + " sectors...");
-        });
+  describe("when Klingon destroyed by sufficient strike", function() {
+    var klingon;
+    beforeEach(function() {
+      klingon = new Klingon(2000, 200);
+      spyOn(klingon, "destroy");
+      ui.target = klingon;
+      ui.commandParameter = 1000;
+      spyOn(game, "generator").and.returnValue(0);
 
-        it("still subtracts the energy", function() {
-            expect(game.energy).toBe(energyBefore - energyToFire);
-        });
+      game.processCommand(ui);
     });
 
-    describe("when Klingon destroyed by sufficient strike", function() {
-        var klingon;
-        beforeEach(function() {
-            klingon = new Klingon(2000, 200);
-            spyOn(klingon, "destroy");
-            ui.target = klingon;
-            ui.commandParameter = 1000;
-            spyOn(game, "generator").and.returnValue(0);
-
-            game.processCommand(ui);
-        });
-
-        it("reports hit and destroyed", function() {
-            expect(ui.writeLine).toHaveBeenCalledWith("Phasers hit Klingon at 2000 sectors with 500 units");
-            expect(ui.writeLine).toHaveBeenCalledWith("Klingon destroyed!");
-        });
-
-        it("subtracts energy", function() {
-            expect(game.energy).toBe(energyBefore - 1000);
-        });
-
-        it("really destroys", function() {
-            expect(klingon.destroy).toHaveBeenCalled();
-        });
+    it("reports hit and destroyed", function() {
+      expect(ui.writeLine).toHaveBeenCalledWith(
+        "Phasers hit Klingon at 2000 sectors with 500 units"
+      );
+      expect(ui.writeLine).toHaveBeenCalledWith("Klingon destroyed!");
     });
 
-    describe("when damaging Klingon", function() {
-        beforeEach(function() {
-            ui.target = new Klingon(2000, 200);
-            ui.commandParameter = 50;
-            spyOn(game, "generator").and.returnValue(0);
-
-            game.processCommand(ui);
-        });
-
-        it("reports damage", function() {
-            expect(ui.writeLine).toHaveBeenCalledWith("Phasers hit Klingon at 2000 sectors with 25 units");
-            expect(ui.writeLine).toHaveBeenCalledWith("Klingon has 175 remaining");
-        });
-
-        it("subtracts energy", function() {
-            expect(game.energy).toBe(energyBefore - 50);
-        });
+    it("subtracts energy", function() {
+      expect(game.energy).toBe(energyBefore - 1000);
     });
 
+    it("really destroys", function() {
+      expect(klingon.destroy).toHaveBeenCalled();
+    });
+  });
 
-    describe("a defect when firing zero", function() {
-        beforeEach(function() {
-            ui.target = new Klingon(2000, 200);
-            ui.commandParameter = 0;
-            spyOn(game, "generator").and.returnValue(0);
+  describe("when damaging Klingon", function() {
+    beforeEach(function() {
+      ui.target = new Klingon(2000, 200);
+      ui.commandParameter = 50;
+      spyOn(game, "generator").and.returnValue(0);
 
-            game.processCommand(ui);
-        });
-
-        it("reports miscalculated damage!", function() {
-            expect(ui.writeLine).toHaveBeenCalledWith("Phasers hit Klingon at 2000 sectors with 1 units");
-            expect(ui.writeLine).toHaveBeenCalledWith("Klingon has 199 remaining");
-        });
-
-        it("mistakenly doesn'torpedoes subtract that one unit", function() {
-            expect(game.energy).toBe(energyBefore);
-        });
+      game.processCommand(ui);
     });
 
+    it("reports damage", function() {
+      expect(ui.writeLine).toHaveBeenCalledWith(
+        "Phasers hit Klingon at 2000 sectors with 25 units"
+      );
+      expect(ui.writeLine).toHaveBeenCalledWith("Klingon has 175 remaining");
+    });
 
+    it("subtracts energy", function() {
+      expect(game.energy).toBe(energyBefore - 50);
+    });
+  });
+
+  describe("a defect when firing zero", function() {
+    beforeEach(function() {
+      ui.target = new Klingon(2000, 200);
+      ui.commandParameter = 0;
+      spyOn(game, "generator").and.returnValue(0);
+
+      game.processCommand(ui);
+    });
+
+    it("reports miscalculated damage!", function() {
+      expect(ui.writeLine).toHaveBeenCalledWith(
+        "Phasers hit Klingon at 2000 sectors with 1 units"
+      );
+      expect(ui.writeLine).toHaveBeenCalledWith("Klingon has 199 remaining");
+    });
+
+    it("mistakenly doesn'torpedoes subtract that one unit", function() {
+      expect(game.energy).toBe(energyBefore);
+    });
+  });
 });
