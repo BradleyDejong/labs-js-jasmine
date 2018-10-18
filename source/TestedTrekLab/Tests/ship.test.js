c30a02790049b78b6a9049af1dfb335688ee2c7c
m081632 <Vaneps.Michael@mayo.edu>
2018-10-18 09:37:05 -0500

    add class

--- source/TestedTrekLab/Tests/ship.test.js
+++ source/TestedTrekLab/Tests/ship.test.js
@@ -3,6 +3,6 @@ import { Ship } from "../StarTrek/ship";
 describe("ship", () =>{
     it("damages subsystem when shield buckles", () => {
         const ship = new Ship();
-        
+        // do the test
     });
 });
