ca9fe929b5fe6927f944ae5da1d6c878eccc0f55
Stageberg <Stageberg.Nicholas@mayo.edu>
2018-10-19 10:36:25 -0500

    missing files

--- source/TestedTrekLab/Tests/ship.test.js
+++ source/TestedTrekLab/Tests/ship.test.js
@@ -6,7 +6,6 @@ describe("ship", () => {
     it("creates a ship", () => {
         const ship = new Ship();
     });
-
     
     it("ship has a shield", () => {
         const ship = new Ship();
@@ -19,8 +18,8 @@ describe("ship", () => {
     });
     it("reserve energy is deducted when energy is transferred to shields", () => {
         const ship = new Ship(10);
-        ship.transferReserveEnergyToShields(5);
-        expect(ship.energySource.remaining).toEqual(5);
+        ship.transferReserveEnergyToShields(6);
+        expect(ship.energySource.remaining).toEqual(4);
     });
     it("ship can be instanced by passing in a shield", () => {
         const ship = new Ship(10, new Shield(10));
@@ -34,9 +33,10 @@ describe("ship", () => {
     });
 
     it("ship can be hit", () => {
-        const ship = new Ship(10, new Shield(10));
+        var shield = new Shield(10);
+        const ship = new Ship(10, shield);
         ship.receiveHit(5);
-        expect(ship.shield.energyRemaining).toEqual(5);
+        expect(shield.energyRemaining).toEqual(5);
     });
 
     it("when damage in excess of shield amount is dealt, shields are damaged", () => {
